ee833dee4e59c26715c278254d17221eea5a8c60
MAMADOU SECK <mamadouseck@Mac-mini-de-MAMADOU.local>
2021-12-13 19:35:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+luxy.init();
+
+//hover effect
+// Arcane
+var hoverDistort1 = new hoverEffect({
+    parent: document.querySelector('#photo1'),
+    intensity: 0.5,
+    image1: 'img/arcane.png',
+    image2: 'img/jinx.jpeg',
+    displacementImage: 'img/displacement.png',
+    imagesRatio: 1.7
+});
+
+//Demon Slayer
+var hoverDistort2 = new hoverEffect({
+    parent: document.querySelector('#photo2'),
+    intensity: 0.5,
+    image1: 'img/demon-slayer.jpeg',
+    image2: 'img/rengoku.jpeg',
+    displacementImage: 'img/displacement.png',
+    imagesRatio: 1.7
+});
+
+// Hunter X Hunter
+var hoverDistort3 = new hoverEffect({
+    parent: document.querySelector('#photo3'),
+    intensity: 0.5,
+    image1: 'img/hxh.jpeg',
+    image2: 'img/killua2.jpeg',
+    displacementImage: 'img/displacement.png',
+    imagesRatio: 1.7
+});
+
+// Tokyo Revengers
+var hoverDistort4 = new hoverEffect({
+    parent: document.querySelector('#photo4'),
+    intensity: 0.5,
+    image1: 'img/tokyo-revengers.jpeg',
+    image2: 'img/mikey.png',
+    displacementImage: 'img/displacement.png',
+    imagesRatio: 1.7
+});
+
+// Kuroku No Basket
+var hoverDistort5 = new hoverEffect({
+    parent: document.querySelector('#photo5'),
+    intensity: 0.5,
+    image1: 'img/kuroku-no-basket.jpeg',
+    image2: 'img/aomine.jpeg',
+    displacementImage: 'img/displacement.png',
+    imagesRatio: 1.7
+});
+
+// My Hero Academia
+var hoverDistort6 = new hoverEffect({
+    parent: document.querySelector('#photo6'),
+    intensity: 0.5,
+    image1: 'img/mha.jpeg',
+    image2: 'img/bakugou.png',
+    displacementImage: 'img/displacement.png',
+    imagesRatio: 1.7
+});
+
+
